6497ab69582d9939b0442e292fdf52deb06d3835
MikaDevy <62851960+MikaDevy@users.noreply.github.com>
2020-06-03 13:06:42 -0700

    Add files via upload

---
+++ en-us/Scripts/JavaScript1.js
@@ -0,0 +1,23 @@
+/* The Code For The Website */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function Subject() {
+
+    var _subjectText = title.text;
+
+    var title = document.getElementById("title").innerText;
+
+}
+
+function Message()
+{
+    var _messageText = message.text;
+
+    var message = document.getElementById("message").innerText;
+}
